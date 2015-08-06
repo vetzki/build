SlimRoms/android_build

Allow Device Specific Headers
This allows a device to define its own headers compared to default

core/binary.mk
@@ -617,6 +617,11 @@ normal_objects := \
 
 all_objects := $(normal_objects) $(gen_o_objects)
 
+## Allow a device's own headers to take precedence over global ones
+ifneq ($(TARGET_SPECIFIC_HEADER_PATH),)
+LOCAL_C_INCLUDES += $(TOPDIR)$(TARGET_SPECIFIC_HEADER_PATH)
+endif
+
 LOCAL_C_INCLUDES += $(TOPDIR)$(LOCAL_PATH) $(intermediates)
 
 ifndef LOCAL_SDK_VERSION

vetzki/build

Conflicts:
	core/binary.mk
Change-Id: I6f9b836646f2ad812d73b3dd2a3bc81a3cebc3e3

core/binary.mk
@@ -818,6 +818,11 @@ normal_objects := \
 
 all_objects := $(normal_objects) $(gen_o_objects)
 
+## Allow a device's own headers to take precedence over global ones
+ifneq ($(TARGET_SPECIFIC_HEADER_PATH),)
+my_c_includes += $(TOPDIR)$(TARGET_SPECIFIC_HEADER_PATH)
+endif
+
 my_c_includes += $(TOPDIR)$(LOCAL_PATH) $(intermediates) $(generated_sources_dir)
 
 ifndef LOCAL_SDK_VERSION